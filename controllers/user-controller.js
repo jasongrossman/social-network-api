4ba396cbe3d3b10242712e00d8514cc65e002f3c
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-10-15 11:39:21 -0400

    added user controller API calls

---
+++ controllers/user-controller.js
@@ -0,0 +1,64 @@
+const { User } = require('../models');
+
+const userController = {
+    //GET all users
+    getAllUsers(req, res) {
+        User.find({})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    //GET a single user by ID
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    //POST a new user
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(400).json(err));
+    },
+    
+    //PUT update a user by ID
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id}, body, {new: true, runValidators: true})
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with that id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    //DELETE user by ID
+    deleteUser({ params}, res) {
+        User.findOneAndDelete({ _id: params.id}) 
+        .then(dbUserData=> {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with that id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    }
+}
+
+module.exports = userController;
