290486b938e8b865cc1ef06fa86768695999d24d
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-10-21 10:07:05 -0400

    added friend routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -58,7 +58,35 @@ const userController = {
             res.json(dbUserData);
         })
         .catch(err => res.status(400).json(err));
-    }
-}
+    },
+
+    //Add a new friend to friends array
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+          { _id: params.userId },
+          { $push: { friends: params.friendId } },
+          { new: true, runValidators: true }
+        )
+          .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No user found with this id!' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => res.json(err));
+      },
+
+      //DELETE friend from friends array
+      deleteFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId } },
+            { new: true }
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+      }
+};
 
 module.exports = userController;
